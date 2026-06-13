fac9f31db7cd16deb1af3bf1c54e0708f1a43b8a
Jake Skiba <35812453+jvskiba@users.noreply.github.com>
2026-06-13 17:04:54 -0400

    Little more reorganizing and added a bat file to build new releases

---
+++ Software/build_release.bat
@@ -0,0 +1,16 @@
+@echo off
+
+cd /d "%~dp0"
+
+pyinstaller ^
+    --clean ^
+    --noconfirm ^
+    --onedir ^
+    --distpath "..\Distribution" ^
+    --workpath ".\build" ^
+    --specpath ".\build" ^
+    src\JvS_Data_Acquisition.py
+
+copy /Y "src\config.json" "..\Distribution\JvS_Data_Acquisition\"
+
+pause
